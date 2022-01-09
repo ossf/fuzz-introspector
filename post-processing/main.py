17a492ab82dc40cd4d5747e854a52842be3c417d
David Korczynski <david@adalogics.com>
2022-01-09 23:51:30 +0000

    post-process: refactor main

--- post-processing/main.py
+++ post-processing/main.py
@@ -26,14 +26,13 @@ l = logging.getLogger(name=__name__)
 def run_analysis_on_dir(target_folder,
         git_repo_url,
         coverage_url):
-    # Load all the data needed
     l.info("[+] Loading profiles")
     profiles = fuzz_data_loader.load_all_profiles(target_folder)
+
     l.info("[+] Accummulating profiles")
     for profile in profiles:
         profile.accummulate_profile(target_folder)
 
-    # Merge all profiles into a project profile    
     l.info("[+] Creating project profile")
     project_profile = fuzz_data_loader.MergedProjectProfile(profiles)
 
@@ -41,40 +40,35 @@ def run_analysis_on_dir(target_folder,
     for profile in profiles:
         profile.refine_paths(project_profile.basefolder)
 
-    # Create the HTML report that can be viewed.
-    if coverage_url == "":
-        coverage_url = "http://localhost:8008/covreport/linux"
-
     l.info("[+] Creating HTML report")
-    fuzz_html.create_html_report(profiles, project_profile, coverage_url, git_repo_url, project_profile.basefolder)
-
+    fuzz_html.create_html_report(
+            profiles,
+            project_profile,
+            coverage_url,
+            git_repo_url,
+            project_profile.basefolder)
 
 def create_parser():
     parser = argparse.ArgumentParser()
-
     parser.add_argument("--target_dir", 
                         type=str,
                         help="Directory where the data files are",
                         required=True)
-    
     parser.add_argument('--git_repo_url', 
                         type=str,
                         help="Git repository with the source code",
                         default="")
-        
     parser.add_argument('--coverage_url', 
                         type=str,
                         help="URL with coverage information", 
-                        default="")
-
+                        default="http://localhost:8008/covreport/linux")
     args = parser.parse_args()
+
     return args
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     l.info("Running fuzz introspector post-processing")
-
+    logging.basicConfig(level=logging.INFO)
     args = create_parser()
-
     run_analysis_on_dir(args.target_dir, args.git_repo_url, args.coverage_url)
     l.info("Ending fuzz introspector post-processing")
