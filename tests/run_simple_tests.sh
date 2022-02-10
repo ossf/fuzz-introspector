905f01ebeb21d0f61c4349238247c1215a66a0be
David Korczynski <david@adalogics.com>
2022-02-10 17:15:11 +0000

    setup simple test script
    This uses much of the examples infra but is more geared towards scripts
    that can be used to quickly verify if all tests pass.

---
+++ tests/run_simple_tests.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -u
+# Copyright 2021 Fuzz Introspector Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+ROOT=$PWD
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+for PROJ in simple-example-0 simple-example-1 simple-example-2 simple-example-3 simple-example-4 simple-example-indirect-pointers; do
+  echo "Testing $PROJ"
+  cd ${ROOT}/${PROJ}
+  rm -rf ./web
+
+  # Check compilation
+  ./build_all.sh > log_compilation.txt 2>&1
+  retval=$?
+  if [ $retval -ne 0 ]; then
+    echo -e "${RED}[-] fail: compilation${NC}"
+    continue
+  fi
+  
+  # Check post-processing
+  mkdir web
+  cd web
+  python3 ${ROOT}/../post-processing/main.py --target_dir=../ > log_post-processing.txt 2>&1
+  retval=$?
+  if [ $retval -ne 0 ]; then
+    echo -e "${RED}[-] fail: post-processing${NC}"
+    continue
+  fi
+
+  echo -e "${GREEN}[+] success${NC}"
+done
