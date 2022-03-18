dd83297970f75a31b9efca17448523c72fe9c532
AdamKorcz <Adam@adalogics.com>
2022-03-18 22:39:18 +0000

    Add feature to hide C std lib funcs in calltree

--- post-processing/styling/calltree.js
+++ post-processing/styling/calltree.js
@@ -22,7 +22,65 @@ $( document ).ready(function() {
     createNavBar();
     addFuzzBlockerLines();
     addExpandSymbols();
+
+    document.addEventListener('click', function(e) {
+      e = e || window.event;
+      var target = e.target;
+      var menuElement = document.getElementById("std-lib-functions");
+      if(isDescendant(menuElement, target)) {
+         e.stopPropagation();
+      }
+  }, false);
+
+
+
+    const checkbox = document.getElementById('free-chckbox');
+    checkbox.addEventListener('change', (event) => {
+      hideNodesWithText("free")
+    })
+
+    const checkbox2 = document.getElementById("abort-chckbox");
+    checkbox2.addEventListener('change', (event) => {
+      hideNodesWithText("abort")
+    })
+
+    const checkbox3 = document.getElementById("malloc-chckbox");
+    checkbox3.addEventListener('change', (event) => {
+      hideNodesWithText("malloc")
+    })
+
+    const checkbox4 = document.getElementById("calloc-chckbox");
+    checkbox4.addEventListener('change', (event) => {
+      hideNodesWithText("calloc")
+    })
+
+    const checkbox5 = document.getElementById("exit-chckbox");
+    checkbox5.addEventListener('change', (event) => {
+      hideNodesWithText("exit")
+    })
+
+    const checkbox6 = document.getElementById("memcmp-chckbox");
+    checkbox6.addEventListener('change', (event) => {
+      hideNodesWithText("memcmp")
+    })
+
+    const checkbox7 = document.getElementById("strlen-chckbox");
+    checkbox7.addEventListener('change', (event) => {
+      hideNodesWithText("strlen")
+    })
 });
+
+
+function isDescendant(parent, child) {
+     var node = child.parentNode;
+     while (node != null) {
+         if (node == parent) {
+             return true;
+         }
+         node = node.parentNode;
+     }
+     return false;
+}
 
 function addFuzzBlockerLines() {
   var coverageLines;
@@ -37,6 +95,12 @@ function addFuzzBlockerLines() {
       coverageLines[j].append(infoBtn);
     }
   }
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 function createNavBar() {
@@ -71,7 +135,63 @@ function createNavBar() {
     btn3.id = "collapse-all-button"
     btn3.innerText = "Collapse all";
     e.append(btn3);
+
+    let btn4 = document.createElement("span");
+    btn4.classList.add("calltree-nav-btn2");
+    btn4.id = "std-lib-functions";
+    btn4.innerHTML = `<div class="dropdown">
+      <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+      <div id="myDropdown" class="dropdown-content">
+        <div style="display:flex">
+          <div style="flex:1"><input type="checkbox" name="free-chckbox" id="free-chckbox" class="shown-checkbox" checked></div>
+          <div style="flex:3">free</div>
+        </div>
+        <div style="display:flex">
+          <div style="flex:1"><input type="checkbox" name="abort-chckbox" id="abort-chckbox" class="shown-checkbox" checked></div>
+          <div style="flex:3">abort</div>
+        </div>
+        <div style="display:flex">
+          <div style="flex:1"><input type="checkbox" name="malloc-chckbox" id="malloc-chckbox" class="shown-checkbox" checked></div>
+          <div style="flex:3">malloc</div>
+        </div>
+        <div style="display:flex">
+          <div style="flex:1"><input type="checkbox" name="calloc-chckbox" id="calloc-chckbox" class="shown-checkbox" checked></div>
+          <div style="flex:3">calloc</div>
+        </div>
+        <div style="display:flex">
+          <div style="flex:1"><input type="checkbox" name="exit-chckbox" id="exit-chckbox" class="shown-checkbox" checked></div>
+          <div style="flex:3">exit</div>
+        </div>
+        <div style="display:flex">
+          <div style="flex:1"><input type="checkbox" name="memcmp-chckbox" id="memcmp-chckbox" class="shown-checkbox" checked></div>
+          <div style="flex:3">memcmp</div>
+        </div>
+        <div style="display:flex">
+          <div style="flex:1"><input type="checkbox" name="strlen-chckbox" id="strlen-chckbox" class="shown-checkbox" checked></div>
+          <div style="flex:3">strlen</div>
+        </div>
+      </div>
+    </div>`;
+    e.append(btn4);
+
     document.getElementsByClassName("content-wrapper")[0].prepend(e);
+
+    
+
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
+    
 
     $('#show-idx-button').click(function(){
       $(this).toggleClass("active");
@@ -95,6 +215,15 @@ function createNavBar() {
         $(".coverage-line-inner.collapse-symbol").toggleClass("collapse-symbol expand-symbol");
       });
     })
+}
+
+function hideNodesWithText(text) {
+  $(".coverage-line-inner").each(function( index ) {
+    var funcName = $( this ).find(".language-clike").text().trim()
+    if(funcName===text) {
+      $(this).toggleClass("hidden");
+    }
+  });
 }
 
 function addExpandSymbols() {
