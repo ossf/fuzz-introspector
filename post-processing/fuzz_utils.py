58f329a6185e1bb1220cba0bd887df0686210572
David Korczynski <david@adalogics.com>
2022-04-28 16:27:06 +0100

    nit

--- post-processing/fuzz_utils.py
+++ post-processing/fuzz_utils.py
@@ -134,8 +134,7 @@ def add_to_json_file(
         json_file_path: str,
         fuzzer_name: str,
         key: str,
-        val: str
-        ):
+        val: str):
     # Create file if it does not exist
     if not os.path.isfile(json_file_path):
         json_data = dict()
