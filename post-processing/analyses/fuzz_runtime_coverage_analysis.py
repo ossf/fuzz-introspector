0806b7310cbbd682e2a2e3bfbd383f5c3694c930
David Korczynski <david@adalogics.com>
2022-05-12 23:46:45 +0100

    analyses: extend and refactor runtime coverage
    Add a new column that shows which fuzzers reach each of the
    functions with high complexity but low runtime coverage.
    
    Also refactors some of the code and adds static types.

--- post-processing/analyses/fuzz_runtime_coverage_analysis.py
+++ post-processing/analyses/fuzz_runtime_coverage_analysis.py
@@ -33,14 +33,16 @@ class FuzzRuntimeCoverageAnalysis(fuzz_analysis.AnalysisInterface):
     def __init__(self):
         self.name = "RuntimeCoverageAnalysis"
 
-    def analysis_func(self,
-                      toc_list: List[Tuple[str, str, int]],
-                      tables: List[str],
-                      project_profile: fuzz_data_loader.MergedProjectProfile,
-                      profiles: List[fuzz_data_loader.FuzzerProfile],
-                      basefolder: str,
-                      coverage_url: str,
-                      conclusions) -> str:
+    def analysis_func(
+        self,
+        toc_list: List[Tuple[str, str, int]],
+        tables: List[str],
+        project_profile: fuzz_data_loader.MergedProjectProfile,
+        profiles: List[fuzz_data_loader.FuzzerProfile],
+        basefolder: str,
+        coverage_url: str,
+        conclusions
+    ) -> str:
         logger.info(f" - Running analysis {self.name}")
 
         functions_of_interest = self.analysis_coverage_runtime_analysis(
@@ -57,10 +59,12 @@ class FuzzRuntimeCoverageAnalysis(fuzz_analysis.AnalysisInterface):
         )
         html_string += "<p>This section gives analysis based on data about the runtime " \
                        "coverage information</p>"
-        html_string += f"<p>For futher technical details on how this section is made, please " \
-                       f"see the " \
-                       f"<a href=\"{fuzz_constants.GIT_BRANCH_URL}/doc/Glossary.md#runtime" \
-                       f"-coverage-analysis\">Glossary</a>.</p>"
+        html_string += (
+            f"<p>For futher technical details on how this section is made, please "
+            f"see the "
+            f"<a href=\"{fuzz_constants.GIT_BRANCH_URL}/doc/Glossary.md#runtime"
+            f"-coverage-analysis\">Glossary</a>.</p>"
+        )
         html_string += fuzz_html_helpers.html_add_header_with_link(
             "Complex functions with low coverage", 3, toc_list)
         tables.append(f"myTable{len(tables)}")
@@ -70,17 +74,25 @@ class FuzzRuntimeCoverageAnalysis(fuzz_analysis.AnalysisInterface):
                 ("Func name", ""),
                 ("Function total lines", ""),
                 ("Lines covered at runtime", ""),
-                ("percentage covered", "")
-            ])
+                ("percentage covered", ""),
+                ("Reached by fuzzers", "")
+            ]
+        )
 
         for funcname in functions_of_interest:
             logger.debug(f"Iterating the function {funcname}")
-            total_func_lines, hit_lines = project_profile.runtime_coverage.get_hit_summary(funcname)
+            func_lines, hit_lines = project_profile.runtime_coverage.get_hit_summary(funcname)
+
+            if funcname in project_profile.all_functions:
+                reached_by = str(project_profile.all_functions[funcname].reached_by_fuzzers)
+            else:
+                reached_by = ""
             html_string += fuzz_html_helpers.html_table_add_row([
                 fuzz_utils.demangle_cpp_func(funcname),
-                total_func_lines,
+                func_lines,
                 hit_lines,
-                "%.5s%%" % (str((hit_lines / total_func_lines) * 100.0))
+                "%.5s%%" % (str((hit_lines / func_lines) * 100.0)),
+                reached_by
             ])
         html_string += "</table>"
         html_string += "</div>"  # report-box
@@ -89,9 +101,10 @@ class FuzzRuntimeCoverageAnalysis(fuzz_analysis.AnalysisInterface):
         return html_string
 
     def analysis_coverage_runtime_analysis(
-            self,
-            profiles: List[fuzz_data_loader.FuzzerProfile],
-            merged_profile: fuzz_data_loader.MergedProjectProfile):
+        self,
+        profiles: List[fuzz_data_loader.FuzzerProfile],
+        merged_profile: fuzz_data_loader.MergedProjectProfile
+    ) -> List[str]:
         """
         Identifies the functions that are hit in terms of coverage, but
         only has a low percentage overage in terms of lines covered in the
@@ -105,7 +118,7 @@ class FuzzRuntimeCoverageAnalysis(fuzz_analysis.AnalysisInterface):
         # Find all functions that satisfy:
         # - source lines above 50
         # - less than 15% coverage
-        functions_of_interest = []
+        functions_of_interest: List[str] = []
         for funcname in merged_profile.runtime_coverage.get_all_hit_functions():
             logger.debug(f"Going through {funcname}")
 
