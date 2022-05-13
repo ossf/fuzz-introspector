9932fc5680da5103cba6d9da5aa087544085d0eb
David Korczynski <david@adalogics.com>
2022-05-13 11:10:36 +0100

    catch even another bug, also through typing

--- post-processing/analyses/fuzz_engine_input.py
+++ post-processing/analyses/fuzz_engine_input.py
@@ -172,7 +172,7 @@ class FuzzEngineInputAnalysis(fuzz_analysis.AnalysisInterface):
             json_file_path: str,
             fuzzer_name: str,
             key: str,
-            val: str):
+            val: List[str]):
         # Create file if it does not exist
         if not os.path.isfile(json_file_path):
             json_data = dict()
