b74712934c5adf9c817aeaa4bceaa5b47e6a9b7c
David Korczynski <david@adalogics.com>
2022-06-10 15:24:28 +0100

    frontends: python: refactor handler script
    This is maturing and code quality should follow.
    
    Ref: https://github.com/ossf/fuzz-introspector/issues/280

--- frontends/python/main.py
+++ frontends/python/main.py
@@ -15,38 +15,17 @@
 import os
 import sys
 import json
+import logging
 import argparse
+
+from typing import List, Set
 
 from pycg.pycg import CallGraphGenerator
 from pycg import formats
 from pycg.utils.constants import CALL_GRAPH_OP
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--fuzzer",
-        help="Fuzzer to be processed"
-    )
-    parser.add_argument(
-        "--package",
-        help="Package containing the code to be analyzed",
-        default=None
-    )
-    parser.add_argument(
-        "--sources",
-        help="Extra help for identifying sources",
-        default=[],
-        nargs="+"
-    )
-    parser.add_argument(
-        "--scan",
-        default=False,
-        help="Set if package should be scanned for sources",
-        action="store_true"
-    )
-    args = parser.parse_args()
-    run_fuzz_pass(args.fuzzer, args.package, args.sources, args.scan)
-    print("Done running pass")
+logger = logging.getLogger(name=__name__)
+
 
 def resolve_package(fuzzer_path):
     """Resolves the package of a fuzzer"""
@@ -78,13 +57,19 @@ def resolve_package(fuzzer_path):
     print("Could not identify the package")
     return None
 
-def run_fuzz_pass(fuzzer, package, sources, scan):
+
+def run_fuzz_pass(
+    fuzzer: str,
+    package: str,
+    sources: List[str],
+    scan: bool
+) -> int:
+    """Runs entire fuzz pass"""
     if package is None:
         package = resolve_package(fuzzer)
         if package is None:
-            print("No package. Exiting early now as the results will not be good")
+            logger.error("No package. Exiting early now as the results will not be good")
             package=""
-            #sys.exit(1)
 
     # Check if we should scan
     scanned_sources = []
@@ -98,37 +83,59 @@ def run_fuzz_pass(fuzzer, package, sources, scan):
                 continue
             scanned_sources.append(full_path)
 
-    print("Fuzzer: %s"%(fuzzer))
-    print("Package: %s"%(package))
+    logger.info(
+        f"Running analysis with arguments: {{fuzzer: {fuzzer}, package: {package} }}"
+    )
     cg = CallGraphGenerator(
         [ fuzzer ] + sources + scanned_sources,
         package,
         -1,
         CALL_GRAPH_OP
     )
-    print("Analysing1")
     cg.analyze()
-    print("Analysing2")
-
     formatter = formats.Fuzz(cg)
     cg_extended = formatter.generate()
 
+    # Post analysis
+    res = post_analysis(cg_extended, fuzzer)
+    if res == None:
+        logger.error("Could not convert calltree to string.")
+        return 1
+
+    fuzzer_name, translated_cg, calltree = res
+
+    # Dump the data we collected
+    dump_fuzz_logic(fuzzer_name, translated_cg, calltree)
+    return 0
+
+
+def post_analysis(cg_extended, fuzzer):
+    """Converts an extended callgraph into data fuzz-introspector
+    post-processing can use.
+    """
     calltree, max_depth = convert_to_fuzzing_cfg(cg_extended)
     if calltree == None:
         print("Could not convert calltree to string. Exiting")
-        sys.exit(1)
+        return None
 
-    # Do analysis on the data
-    converge_reachables(cg_extended)
-    find_all_uses(cg_extended)
+    # Compute fields in the extended callgraph
+    set_all_reachables(cg_extended)
+    set_all_uses(cg_extended)
 
-    translated_cg = translate_cg(cg_extended, fuzzer)
+    # Convert extended callgraph to data fuzz-introspector
+    # post-processing understands.
+    translated_cg = convert_cg_to_introspector_data(cg_extended, fuzzer)
 
+    # Simple way to create a fuzzer name for now. This way of
+    # doing it means we can't have multiple fuzzers in the same
+    # file (at least they will have same name).
     fuzzer_name = os.path.basename(fuzzer).replace(".py", "")
-    dump_fuzz_logic(fuzzer_name, translated_cg, calltree)
 
-def find_all_uses(cg_extended):
-    """Sets functionUses"""
+    return fuzzer_name, translated_cg, calltree
+
+
+def set_all_uses(cg_extended) -> None:
+    """For each element in the extended CG, set the functionUses"""
     for elem in cg_extended['cg']:
         all_uses = []
         for elem2 in cg_extended['cg']:
@@ -138,29 +145,27 @@ def find_all_uses(cg_extended):
                 all_uses.append(elem2)
         cg_extended['cg'][elem]['all_uses'] = len(all_uses)
 
-def extract_local_reachables(elem):
-    print(elem)
-    s = set()
-    for dst in elem['dsts']:
-        s.add(dst['dst'])
-    return s
 
-def converge_reachables(cg_extended):
-    """For each of the elements in the cg_extended cg we converge their reachables"""
+def set_all_reachables(cg_extended):
+    """For each of the elements in the cg_extended cg we converge their reachables.
+    The result of this is that the 'all_reachables' key in the elems dictionary
+    will be a set with the all reachable elements by the given element
+    """
     for elem in cg_extended['cg']:
-        print(f"Converging {elem}")
-        # 'dsts': [{'dst': '<builtin>.type'
+        logger.info(f"Converging {elem}")
         all_reachables = set()
-        ws = extract_local_reachables(cg_extended['cg'][elem])
+        ws = { dst['dst'] for dst in cg_extended['cg'][elem]['dsts'] }
         while len(ws) > 0:
             e1 = ws.pop()
             if e1 not in all_reachables:
                 all_reachables.add(e1)
-                ws = ws.union(extract_local_reachables(cg_extended['cg'][e1]))
-        #print(elem)
+                ws = ws.union(
+                    {dst['dst'] for dst in cg_extended['cg'][e1]['dsts']}
+                )
         cg_extended['cg'][elem]['all_reachables'] = list(all_reachables)
 
-def translate_cg(cg_extended, fuzzer_filename):
+
+def convert_cg_to_introspector_data(cg_extended, fuzzer_filename):
     """Converts the PyCG data into fuzz-introspector data"""
     new_dict = dict()
     new_dict['Fuzzer filename'] = fuzzer_filename
@@ -209,53 +214,124 @@ def dump_fuzz_logic(fuzzer_name, cg_extended, calltree):
     calltree_file = prefix + fuzzer_name + ".data"
     fuzzer_func_data = prefix + fuzzer_name + ".data.yaml"
 
+    # Writes the calltree file
     with open(calltree_file, "w+") as cf:
         cf.write(calltree)
 
+    # Writes the yaml file containing information about all functions etc.
     with open(fuzzer_func_data, "w+") as ffdf:
         ffdf.write(yaml.dump(cg_extended))
 
 
 def convert_to_fuzzing_cfg(cg_extended):
-    """Utility to translate the CG to something fuzz-introspector post-processing
-    can use"""
-    print("Printing CFG output")
+    """Translate a CG to something fuzz-introspector
+    post-processing can use.
+    """
     if "ep" not in cg_extended:
-        print("No entrypoints found")
+        logger.error(
+            "No entrypoints in the CG. This can not be made into fuzzer callgraph"
+        )
         return None
 
-    # Extract fuzzer entrypoint and print calltree.
+    # Extract fuzzer entrypoint
     ep_key = cg_extended['ep']['mod'] + "." + cg_extended['ep']['name']    
     ep_node = cg_extended['cg'][ep_key]
-    #print(json.dumps(cg_extended, indent=4))
-    calltree = "Call tree\n"
-    calltree2, max_depth = get_calltree_as_str(cg_extended['cg'], ep_key, set())
-    calltree = calltree + calltree2
-    #print(calltree)
+
+    # Get calltree as string
+    calltree, max_depth = get_calltree_as_str(
+        cg_extended['cg'],
+        ep_key,
+        set()
+    )
+    # prefix calltree
+    calltree = "Call tree\n" + calltree
     return calltree, max_depth
 
-def get_calltree_as_str(cg_extended, k, s1, depth=0, lineno=-1, themod="", ext_mod=""):
-    """Prints a calltree where k is the key in the cg of the root"""
 
-    #strline = "%s%s src_mod=%s src_linenumber=%d dst_mod=%s\n"%(" "*(depth*2), k, themod, lineno, ext_mod)
-    if themod == "":
-        themod="/"
-    strline = "%s%s %s %d\n"%(" "*(depth*2), k, themod, lineno)
-    #strline = "%s%s src_mod=%s src_linenumber=%d dst_mod=%s\n"%(" "*(depth*2), k, themod, lineno, ext_mod)
-    #print("%s%s src_mod=%s src_linenumber=%d dst_mod=%s"%(" "*(depth*2), k, themod, lineno, ext_mod))
-    sorted_keys = sorted(cg_extended[k]['dsts'], key=lambda x: x['lineno'])
+def get_calltree_as_str(
+    cg_extended,
+    key,
+    visited,
+    depth=0,
+    lineno=-1,
+    key_mod=""
+):
+    """Coverts a calltree into a string.
+
+    key is the root from this will convert to a string. First, the
+    function converts the values related to k into a string, and
+    then iterates through k's children and to perform the same operation.
+
+    This is a recursive function and each key will only be handled once.
+    """
+
+    if key_mod == "":
+        key_mod = "/"
+    strline = "%s%s %s %d\n"%(" "*(depth*2), key, key_mod, lineno)
+    sorted_keys = sorted(cg_extended[key]['dsts'], key=lambda x: x['lineno'])
 
     # Avoid deep recursions
-    if k in s1:
+    if key in visited:
         return strline, depth
 
-    s1.add(k)
+    visited.add(key)
     next_depth = depth
-    for dst in cg_extended[k]['dsts']:
-        tmps, m_depth = get_calltree_as_str(cg_extended, dst['dst'], s1, depth+1, dst['lineno'], dst['mod'], dst['ext_mod'])
+    for dst in cg_extended[key]['dsts']:
+        tmps, m_depth = get_calltree_as_str(
+            cg_extended,
+            dst['dst'],
+            visited,
+            depth = depth+1,
+            lineno = dst['lineno'],
+            key_mod = dst['mod']
+        )
         next_depth = max(m_depth, next_depth)
         strline += tmps
 
     return strline, next_depth
+
+
+def get_cmdline_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--fuzzer",
+        help="Fuzzer to be processed"
+    )
+    parser.add_argument(
+        "--package",
+        help="Package containing the code to be analyzed",
+        default=None
+    )
+    parser.add_argument(
+        "--sources",
+        help="Extra help for identifying sources",
+        default=[],
+        nargs="+"
+    )
+    parser.add_argument(
+        "--scan",
+        default=False,
+        help="Set if package should be scanned for sources",
+        action="store_true"
+    )
+    return parser
+
+
+def main() -> int:
+    logging.basicConfig(level=logging.INFO)
+    logger.info("Starting analysis")
+    parser = get_cmdline_parser()
+    args = parser.parse_args()
+    exit_code = run_fuzz_pass(
+        args.fuzzer,
+        args.package,
+        args.sources,
+        args.scan
+    )
+    logger.info(f"Done running pass. Exit code: {exit_code}")
+
+    return exit_code
+
+
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
