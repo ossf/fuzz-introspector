69d88bd1ebc6fe3ed62d78e9244e0172d361f11b
DavidKorczynski <david@adalogics.com>
2022-07-11 22:13:13 +0100

    test: add fuzzing set up (#389)
    * test: add fuzzing set up
    
    Add fuzzing set up targetting calltree loading. The next step here is to
    enable fuzzing of the high level commands.
    
    * nit
    
    * move fuzz directory to be subdirectory of test dir
    
    * refine paths
    
    * add gitignore file

---
+++ src/test/fuzz/fuzz_cfg_load.py
@@ -0,0 +1,49 @@
+# Copyright 2022 Fuzz Introspector Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Fuzz cfg_load.py"""
+
+import os
+import sys
+import atheris
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../")
+
+from fuzz_introspector import cfg_load  # noqa: E402
+
+
+@atheris.instrument_func
+def TestOneInput(data):
+    """Fuzz cfg_load.data_file_read_calltree"""
+    cfg_file = "test_file.data"
+    with open(cfg_file, "wb") as f:
+        f.write(data)
+
+    # Read the file as a calltree
+    try:
+        cfg_load.data_file_read_calltree(cfg_file)
+    except UnicodeDecodeError:
+        pass
+
+    if os.path.isfile(cfg_file):
+        os.remove(cfg_file)
+
+
+def main():
+    atheris.instrument_all()
+    atheris.Setup(sys.argv, TestOneInput, enable_python_coverage=True)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
