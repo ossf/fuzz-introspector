420f1fc6870c5b3ecdabc18aefeb5f6998401a78
Arthur Chan <arthur.chan@adalogics.com>
2022-08-05 17:51:38 +0100

    Fix formatting

--- src/test/test_branch_profile.py
+++ src/test/test_branch_profile.py
@@ -18,7 +18,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../")
 
-from fuzz_introspector.datatypes import branch_profile
+from fuzz_introspector.datatypes import branch_profile # noqa: E402
 
 
 def test_branch_profile_assign_from_yaml_elem():
