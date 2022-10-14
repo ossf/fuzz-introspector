29ab44ed777f6ed811acd33980dd6199a1586158
Arthur Chan <arthur.chan@adalogics.com>
2022-10-14 17:42:08 +0100

    Add callgraph generation code using soot framework
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

---
+++ frontends/java/soot/run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Process arguments
+JARFILE=
+ENTRYCLASS=
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -j|--jarfile)
+      JARFILE="$2"
+      shift
+      shift
+      ;;
+    -c|--entryclass)
+      ENTRYCLASS="$2"
+      shift
+      shift
+      ;;
+    -m|--entrymethod)
+      ENTRYMETHOD="$2"
+      shift
+      shift
+      ;;
+    *)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z $JARFILE ]
+then
+    echo "You need to specify target with -j <jar_file> or --jarfile <jar_file>."
+    exit 1
+fi
+if [ -z $ENTRYCLASS ]
+then
+    echo "You need to specify entry class with -c <entry_class> or --entryclass <entry_class>."
+    exit 1
+fi
+if [ -z $ENTRYMETHOD ]
+then
+    echo "You need to specify entry class with -m <entry_method> or --entrymethod <entry_method>."
+    exit 1
+fi
+
+# Build and execute the call graph generator
+mvn clean package
+java -Xmx6144M -cp "target/ossf.fuzz.introspector.soot-1.0.jar:$JARFILE" ossf.fuzz.introspector.soot.CallGraphGenerator $ENTRYCLASS $ENTRYMETHOD
