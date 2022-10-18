597f4babc6e2a1288d484b4bb49ab782e4212d54
David Korczynski <david@adalogics.com>
2022-10-18 17:17:52 +0100

    nit
    Signed-off-by: David Korczynski <david@adalogics.com>

--- src/fuzz_introspector/analyses/calltree_analysis.py
+++ src/fuzz_introspector/analyses/calltree_analysis.py
@@ -179,9 +179,10 @@ class Analysis(analysis.AnalysisInterface):
         # visualisation happens in javascript rather than here.
         calltree_html_section_string += "<div id=\"side-overview-wrapper\"></div>"
 
-        logger.info("calltree_html_section_string: <divs>: %d -- </divs>: %d" % (
-            calltree_html_section_string.count("<div"),
-            calltree_html_section_string.count("</div>")
+        logger.info(
+            "calltree_html_section_string: <divs>: %d -- </divs>: %d" % (
+                calltree_html_section_string.count("<div"),
+                calltree_html_section_string.count("</div>")
             )
         )
 
