b84e3dbfd9541b9b03149294195abbb1b626719a
Arthur Chan <arthur.chan@adalogics.com>
2022-10-19 18:37:14 +0100

    Fix build logic with dependency
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- tests/java/test3/build.sh
+++ tests/java/test3/build.sh
@@ -2,5 +2,9 @@
 
 rm -f ./*.class
 rm -f ./*.jar
-javac -cp ../jazzer_api_deploy.jar *.java
+rm -f ./jazzer*
+wget https://github.com/CodeIntelligenceTesting/jazzer/releases/download/v0.12.0/jazzer-linux-x86_64.tar.gz
+tar -zxvf jazzer-linux-x86_64.tar.gz
+javac -cp jazzer_api_deploy.jar *.java
 jar cfv test3.jar *.class
+rm -rf ./jazzer*
