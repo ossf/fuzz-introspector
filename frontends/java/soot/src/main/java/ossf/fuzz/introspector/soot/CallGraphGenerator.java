a0247446b06cff3b750b1dc267e74e96be74380e
Arthur Chan <arthur.chan@adalogics.com>
2022-10-25 16:48:02 +0100

    Fix bug
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- frontends/java/soot/src/main/java/ossf/fuzz/introspector/soot/CallGraphGenerator.java
+++ frontends/java/soot/src/main/java/ossf/fuzz/introspector/soot/CallGraphGenerator.java
@@ -1,4 +1,3 @@
-
 // Copyright 2022 Fuzz Introspector Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -149,7 +148,7 @@ class CustomSenceTransformer extends SceneTransformer {
 						c.getName().equals(this.entryClassStr)) {
 					this.entryMethod = m;
 				}
-        
+
 				// Discover method related information
 				FunctionElement element= new FunctionElement();
 
@@ -241,14 +240,6 @@ class CustomSenceTransformer extends SceneTransformer {
 						blockGraph.getHeads(), 0));
 
 				methodConfig.addFunctionElement(element);
-
-				// Only methods in the entry class or method reachable
-				// from the entry method in the entry class are included
-				// in the call graph result.
-				if (c.getName().equals(this.entryClass) ||
-						element.getFunctionUses() > 0) {
-					methodMap.put(c.getName() + "#" + m.getName(), m);
-				}
 			}
 			classConfig.setFunctionConfig(methodConfig);
 			classYaml.add(classConfig);
@@ -265,7 +256,6 @@ class CustomSenceTransformer extends SceneTransformer {
 			}
 		}
 	}
-
 
 	private Integer calculateDepth(CallGraph cg, SootMethod method) {
 		int depth = 0;
@@ -355,4 +345,3 @@ class CustomSenceTransformer extends SceneTransformer {
 		return excludeList;
 	}
 }
-
