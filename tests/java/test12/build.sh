8a1b1dfb74749e8edf68f45c7242b631ccc248e8
Arthur Chan <arthur.chan@adalogics.com>
2022-11-04 17:07:12 +0000

    Add and fix test case config
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- tests/java/test12/build.sh
+++ tests/java/test12/build.sh
@@ -16,12 +16,12 @@
 rm -f ./*.class
 rm -f ./*.jar
 rm -f ./jazzer*
-wget https://repo1.maven.org/maven2/com/github/javaparser/javaparser-core/3.24.7/javaparser-core-3.24.7.jar
+wget https://repo1.maven.org/maven2/javassist/javassist/3.12.1.GA/javassist-3.12.1.GA.jar
 wget https://github.com/CodeIntelligenceTesting/jazzer/releases/download/v0.12.0/jazzer-linux-x86_64.tar.gz
 tar -zxvf jazzer-linux-x86_64.tar.gz
-javac -cp jazzer_api_deploy.jar:javaparser-core-3.24.7.jar ./*.java
-unzip -uo javaparser-core-3.24.7.jar
-jar cfv test12.jar ./*.class com
+javac -cp jazzer_api_deploy.jar:javassist-3.12.1.GA.jar ./*.java
+unzip -uo javassist-3.12.1.GA.jar
+jar cfv test12.jar ./*.class javassist
 rm -rf ./jazzer*
-rm -rf ./com*
+rm -rf ./javassist*
 rm -rf ./META-INF
