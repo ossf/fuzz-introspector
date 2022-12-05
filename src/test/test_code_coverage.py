a1c6ffd33ea15f0e42b02e212f41a27fd57767e5
Navid Emamdoost <navid.emamdoost@gmail.com>
2022-12-05 09:57:29 -0800

    fix CI
    Signed-off-by: Navid Emamdoost <navid.emamdoost@gmail.com>

--- src/test/test_code_coverage.py
+++ src/test/test_code_coverage.py
@@ -21,6 +21,7 @@ from fuzz_introspector import code_coverage  # noqa: E402
 
 TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 
+
 def test_load_llvm_coverage():
     """Tests loading llvm coverage from a .covreport file."""
     cov_profile = code_coverage.load_llvm_coverage(TEST_DATA_PATH)
@@ -31,9 +32,9 @@ def test_load_llvm_coverage():
     assert len(cov_profile.coverage_files) == 1
     assert len(cov_profile.dual_file_map) == 0
 
-    assert cov_profile.covmap['BZ2_bzCompress'][0] ==  (408, 46800)
-    assert cov_profile.covmap['BZ2_bzCompress'][7] ==  (416, 93600)
-    assert cov_profile.covmap['BZ2_bzCompress'][10] ==  (420, 0)
+    assert cov_profile.covmap['BZ2_bzCompress'][0] == (408, 46800)
+    assert cov_profile.covmap['BZ2_bzCompress'][7] == (416, 93600)
+    assert cov_profile.covmap['BZ2_bzCompress'][10] == (420, 0)
     assert cov_profile.covmap['add_pair_to_block'][0] == (217, 3600000)
     assert cov_profile.covmap['add_pair_to_block'][4] == (221, 1440000)
     assert cov_profile.covmap['add_pair_to_block'][11] == (228, 3510000)
@@ -44,5 +45,5 @@ def test_load_llvm_coverage():
     assert cov_profile.branch_cov_map['BZ2_bzCompress:425,20'] == [0, 0]
     assert cov_profile.branch_cov_map['BZ2_bzCompress:443,14'] == [0, 0]
     assert cov_profile.branch_cov_map['add_pair_to_block:220,16'] == [1440000, 3600000]
-    assert cov_profile.branch_cov_map['add_pair_to_block:224,4'] == [32600, 3600000, 32600, 3510000, 1570000]
-
+    assert cov_profile.branch_cov_map['add_pair_to_block:224,4'] == (
+        [32600, 3600000, 32600, 3510000, 1570000])
