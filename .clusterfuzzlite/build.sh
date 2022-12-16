2f6dca84b12aa1a1b8e8d002018c90d5652098e1
David Korczynski <david@adalogics.com>
2022-12-16 06:40:57 -0800

    nit
    Signed-off-by: David Korczynski <david@adalogics.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -10,7 +10,7 @@ hidden_imports="--hidden-import=yaml \
                --hidden-import=bs4 \
                --hidden-import=matplotlib"
 #fuzzers="test_fuzz_cfg_load.py test_fuzz_report_generation.py"
-fuzzers="test_fuzz_cfg_load.py test_fuzz_cfg_load.py"
+fuzzers="test_fuzz_report_generation.py test_fuzz_report_generation.py"
 for fuzzer in $fuzzers; do
   compile_python_fuzzer $fuzzer -F --add-data "src/fuzz_introspector:fuzz_introspector" $hidden_imports
 done
