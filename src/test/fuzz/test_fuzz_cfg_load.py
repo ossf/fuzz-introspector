876f8fef66e1e1300e2be676c2c808ddee3d84ca
David Korczynski <david@adalogics.com>
2022-12-16 05:49:59 -0800

    fix
    Signed-off-by: David Korczynski <david@adalogics.com>

--- src/test/fuzz/test_fuzz_cfg_load.py
+++ src/test/fuzz/test_fuzz_cfg_load.py
@@ -34,7 +34,7 @@ from fuzz_introspector import exceptions  # noqa: E402
 @atheris.instrument_func
 def test_TestOneInput(data):
     """Fuzz cfg_load.data_file_read_calltree"""
-    cfg_file = "test_file.data"
+    cfg_file = "/tmp/test_file.data"
     with open(cfg_file, "wb") as f:
         f.write(data)
 
