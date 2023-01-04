1decee8f1572381d7e1ffc7d7095baaa192cf31d
David Korczynski <david@adalogics.com>
2023-01-04 09:02:47 -0800

    add function-level comments
    Signed-off-by: David Korczynski <david@adalogics.com>

--- src/fuzz_introspector/json_report.py
+++ src/fuzz_introspector/json_report.py
@@ -28,6 +28,7 @@ logger = logging.getLogger(name=__name__)
 
 
 def _get_summary_dict() -> Dict[Any, Any]:
+    """Returns the current json report on disk as a dictionary."""
     if not os.path.isfile(constants.SUMMARY_FILE):
         existing_contents = dict()
     else:
@@ -38,6 +39,9 @@ def _get_summary_dict() -> Dict[Any, Any]:
 
 
 def _overwrite_report_with_dict(new_dict: Dict[Any, Any]) -> None:
+    """Writes `new_dict` as contents to the report on disk. Will overwrite any
+    contents of the existing report.
+    """
     # Write back the json file
     with open(constants.SUMMARY_FILE, 'w') as report_fd:
         json.dump(dict(new_dict), report_fd)
@@ -47,7 +51,11 @@ def add_analysis_dict_to_json_report(
     analysis_name: str,
     dict_to_add: Dict[Any, Any]
 ) -> None:
-    """Wraps dictionary into an appropriate format"""
+    """Wraps dictionary into an appropriate format
+
+    Will overwrite the existing key/value pair for the analysis if it already
+    exists as an analysis in the report.
+    """
     contents = _get_summary_dict()
     if 'analyses' not in contents:
         contents['analyses'] = dict()
@@ -60,7 +68,10 @@ def add_analysis_json_str_as_dict_to_report(
     analysis_name: str,
     json_str: str
 ) -> None:
-    """Converts a json string to a dictionary and add it to the report"""
+    """Converts a json string to a dictionary and add it to the report.
+
+    Will overwrite the existing key/value pair for the analysis if it already
+    exists as an analysis in the report."""
     add_analysis_dict_to_json_report(analysis_name, json.loads(json_str))
 
 
@@ -69,7 +80,11 @@ def add_fuzzer_key_value_to_report(
     key: str,
     value: Any
 ) -> None:
-    """Add the key/value pair to the json report under the fuzzer key"""
+    """Add the key/value pair to the json report under the fuzzer key.
+
+    Will overwrite the existing key/value pair under the fuzzer if it already
+    exists in the report.
+    """
     contents = _get_summary_dict()
 
     if fuzzer_name not in contents:
