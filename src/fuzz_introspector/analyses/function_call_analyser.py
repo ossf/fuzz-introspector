80ba4b4553148e4f93e5080ce8983fbc3988b4a0
David Korczynski <david@adalogics.com>
2023-02-03 09:40:35 -0800

    format function call analyser
    Signed-off-by: David Korczynski <david@adalogics.com>

--- src/fuzz_introspector/analyses/function_call_analyser.py
+++ src/fuzz_introspector/analyses/function_call_analyser.py
@@ -15,21 +15,14 @@
 
 import logging
 
-from typing import (
-    List,
-    Tuple,
-    Dict
-)
+from typing import (List, Tuple, Dict)
 
 from fuzz_introspector import analysis
 from fuzz_introspector import cfg_load
 from fuzz_introspector import html_helpers
 from fuzz_introspector import utils
-from fuzz_introspector.datatypes import (
-    project_profile,
-    fuzzer_profile,
-    function_profile
-)
+from fuzz_introspector.datatypes import (project_profile, fuzzer_profile,
+                                         function_profile)
 
 logger = logging.getLogger(name=__name__)
 
@@ -80,13 +73,9 @@ class ThirdPartyAPICoverageAnalyser(analysis.AnalysisInterface):
 
         return func_file
 
-    def add_callsite_record(
-        self,
-        target_func_list: List[str],
-        func_name: str,
-        source_file_list: List[str],
-        callsites: Dict[str, List[str]]
-    ) -> List[str]:
+    def add_callsite_record(self, target_func_list: List[str], func_name: str,
+                            source_file_list: List[str],
+                            callsites: Dict[str, List[str]]) -> List[str]:
         """This function aims to add all third party function call to its
         source location and line number mapping to a combined dictionary.
         """
@@ -106,23 +95,18 @@ class ThirdPartyAPICoverageAnalyser(analysis.AnalysisInterface):
         return exist_list
 
     def third_party_func_profile(
-        self,
-        profile: project_profile.MergedProjectProfile,
+        self, profile: project_profile.MergedProjectProfile,
         callsites: List[cfg_load.CalltreeCallsite],
         function_list: List[function_profile.FunctionProfile]
-    ) -> Tuple[
-        List[function_profile.FunctionProfile],
-        Dict[str, List[str]],
-        List[str]
-    ]:
+    ) -> Tuple[List[function_profile.FunctionProfile], Dict[str, List[str]],
+               List[str]]:
         # Build up target function list
         target_list = [
-            fd for fd in profile.all_functions.values() if not fd.function_source_file
+            fd for fd in profile.all_functions.values()
+            if not fd.function_source_file
         ]
 
-        target_func_list = [
-            func.function_name for func in target_list
-        ]
+        target_func_list = [func.function_name for func in target_list]
 
         # Add unreachable target functions
         for function in function_list:
@@ -138,17 +122,10 @@ class ThirdPartyAPICoverageAnalyser(analysis.AnalysisInterface):
             func_name = callsite.dst_function_name
             src_file = self.get_source_file(callsite)
             parent_func = self.get_parent_func_name(callsite)
-            src_file_with_line = "%s#%s:%s" % (
-                src_file,
-                parent_func,
-                callsite.src_linenumber
-            )
-            self.add_callsite_record(
-                target_func_list,
-                func_name,
-                [src_file_with_line],
-                callsite_dict
-            )
+            src_file_with_line = "%s#%s:%s" % (src_file, parent_func,
+                                               callsite.src_linenumber)
+            self.add_callsite_record(target_func_list, func_name,
+                                     [src_file_with_line], callsite_dict)
 
         # Discover reachable func calls
         reachable_func_list = []
@@ -157,26 +134,18 @@ class ThirdPartyAPICoverageAnalyser(analysis.AnalysisInterface):
             for func_name in function.callsite.keys():
 
                 reachable_func_list.extend(
-                    self.add_callsite_record(
-                        target_func_list,
-                        func_name,
-                        function.callsite[func_name],
-                        callsite_dict
-                    )
-                )
+                    self.add_callsite_record(target_func_list, func_name,
+                                             function.callsite[func_name],
+                                             callsite_dict))
 
         return target_list, callsite_dict, reachable_func_list
 
-    def analysis_func(
-        self,
-        toc_list: List[Tuple[str, str, int]],
-        tables: List[str],
-        proj_profile: project_profile.MergedProjectProfile,
-        profiles: List[fuzzer_profile.FuzzerProfile],
-        basefolder: str,
-        coverage_url: str,
-        conclusions: List[html_helpers.HTMLConclusion]
-    ) -> str:
+    def analysis_func(self, toc_list: List[Tuple[str, str,
+                                                 int]], tables: List[str],
+                      proj_profile: project_profile.MergedProjectProfile,
+                      profiles: List[fuzzer_profile.FuzzerProfile],
+                      basefolder: str, coverage_url: str,
+                      conclusions: List[html_helpers.HTMLConclusion]) -> str:
         """
         Performs an analysis for all third party API call in the target project.
         Finds the set of third party API call in the project code and provide
@@ -195,78 +164,62 @@ class ThirdPartyAPICoverageAnalyser(analysis.AnalysisInterface):
         callsite_list = []
         function_list = []
         for profile in profiles:
-            callsite_list.extend(cfg_load.extract_all_callsites(profile.function_call_depths))
+            callsite_list.extend(
+                cfg_load.extract_all_callsites(profile.function_call_depths))
             for key in profile.all_class_functions.keys():
                 function_list.append(profile.all_class_functions[key])
-        (func_profile_list, called_func_dict, reachable_func_list) = (
-            self.third_party_func_profile(proj_profile, callsite_list, function_list)
-        )
+        (func_profile_list, called_func_dict,
+         reachable_func_list) = (self.third_party_func_profile(
+             proj_profile, callsite_list, function_list))
 
         html_string = ""
         html_string += "<div class=\"report-box\">"
 
         html_string += html_helpers.html_add_header_with_link(
-            "Function call coverage",
-            1,
-            toc_list
-        )
+            "Function call coverage", 1, toc_list)
 
         # Table with all function calls for each files
         html_string += "<div class=\"collapsible\">"
-        html_string += (
-            "<p>"
-            "This section shows a list of 3rd party function "
-            "calls and their relative coverage information. "
-            "By static analysis of the target project code, "
-            "all of the 3rd party function call and their caller "
-            "information, including the source file and line "
-            "number that initiate the call are captured. "
-            "The caller source code file and line number are "
-            "shown in column 2 while column 1 is the function "
-            "name of the 3rd party function call. Each occurrent "
-            "of the 3rd party function call will occuply "
-            "a separate row. Column 3 of each row indicate if "
-            "the 3rd party call in the source file line is "
-            "unreachable. Column 4 lists all fuzzers that have "
-            "covered that particular system call in "
-            "that specific location (source file and line)"
-            "during their dynamic fuzzing."
-            "</p>"
-        )
+        html_string += ("<p>"
+                        "This section shows a list of 3rd party function "
+                        "calls and their relative coverage information. "
+                        "By static analysis of the target project code, "
+                        "all of the 3rd party function call and their caller "
+                        "information, including the source file and line "
+                        "number that initiate the call are captured. "
+                        "The caller source code file and line number are "
+                        "shown in column 2 while column 1 is the function "
+                        "name of the 3rd party function call. Each occurrent "
+                        "of the 3rd party function call will occuply "
+                        "a separate row. Column 3 of each row indicate if "
+                        "the 3rd party call in the source file line is "
+                        "unreachable. Column 4 lists all fuzzers that have "
+                        "covered that particular system call in "
+                        "that specific location (source file and line)"
+                        "during their dynamic fuzzing."
+                        "</p>")
 
         html_string += html_helpers.html_add_header_with_link(
-            "Function in each files in report",
-            2,
-            toc_list
-        )
+            "Function in each files in report", 2, toc_list)
 
         # Third party function calls table
         tables.append(f"myTable{len(tables)}")
         html_string += html_helpers.html_create_table_head(
             tables[-1],
-            [
-                ("Target sink", ""),
-                ("Callsite location",
-                 "Source file, line number and parent function of this function call. "
-                 "Based on static analysis."),
-                ("Reached by fuzzer",
-                 "Is this code reachable by any functions? "
-                 "Based on static analysis."),
-                ("Covered by Fuzzers",
-                 "The specific list of fuzzers that cover this function call. "
-                 "Based on dynamic analysis.")
-            ]
-        )
+            [("Target sink", ""),
+             ("Callsite location",
+              "Source file, line number and parent function of this function call. "
+              "Based on static analysis."),
+             ("Reached by fuzzer", "Is this code reachable by any functions? "
+              "Based on static analysis."),
+             ("Covered by Fuzzers",
+              "The specific list of fuzzers that cover this function call. "
+              "Based on dynamic analysis.")])
 
         # Loop through each function call exists in this project
         # Filer to only look for functions of interest
         command_injection_sinks = [
-            "system",
-            "execve",
-            "execl",
-            "wordexp",
-            "popen",
-            "fdopen"
+            "system", "execve", "execl", "wordexp", "popen", "fdopen"
         ]
         # Keeping memory unsafe sinks empty for now.
         # memory_unsafe_sinks = [
@@ -291,7 +244,8 @@ class ThirdPartyAPICoverageAnalyser(analysis.AnalysisInterface):
             # Loop through the list of calledlocation for this function
             for called_location in called_location_list:
                 # Determine if the function call in this called location is reachable
-                hit = "Yes" if (called_location in reachable_func_list) else "No"
+                hit = "Yes" if (called_location
+                                in reachable_func_list) else "No"
 
                 # Determine if this called location is covered by any fuzzers
                 fuzzer_hit = False
@@ -304,12 +258,12 @@ class ThirdPartyAPICoverageAnalyser(analysis.AnalysisInterface):
                     if coverage.is_func_lineno_hit(parent_func, lineno):
                         fuzzer_hit = True
                         break
-                list_of_fuzzer_covered = fd.reached_by_fuzzers if fuzzer_hit else [""]
+                list_of_fuzzer_covered = fd.reached_by_fuzzers if fuzzer_hit else [
+                    ""
+                ]
 
                 html_string += html_helpers.html_table_add_row([
-                    f"{func_name}",
-                    f"{called_location}",
-                    f"{hit}",
+                    f"{func_name}", f"{called_location}", f"{hit}",
                     f"{str(list_of_fuzzer_covered)}"
                 ])
         html_string += "</table>"
