f18f786e289784f50d652a6cde55dcb83784b03e
Arthur Chan <arthur.chan@adalogics.com>
2023-02-07 07:08:00 +0800

    JVM Integration: Fix package name discovery for project with fuzzer using package (#802)
    * Fix package name discovery for project with fuzzer using package
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>
    
    * Fix script to use absolute path
    Add comments
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>
    
    * Fix call_process using cwd
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>
    
    * Fix package name retrieve logic
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>
    
    * Fix error param and logic
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>
    
    ---------
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- frontends/java/oss-fuzz-main.py
+++ frontends/java/oss-fuzz-main.py
@@ -54,27 +54,42 @@ def find_fuzz_targets(path):
   For each fuzz target, if there is no {fuzzername}.jar then it is created.
   """
   print("Finding fuzz targets in %s" % path)
-  jar_files = set()
   targets = list()
-  for classfile in os.listdir(path):
-    print("Checking target %s" % classfile)
-    if not classfile.endswith(".class"):
-      continue
-
+  class_file_list = list()
+  for wrapper_script in os.listdir(path):
+    print("Checking target %s" % wrapper_script)
     # Check if wrapper script exists and whether it has the right tag.
-    wrapper_script = classfile.replace(".class", "")
     if not os.path.isfile(wrapper_script):
       continue
     with open(wrapper_script) as wrapper_script_fd:
-      if 'LLVMFuzzerTestOneInput' not in wrapper_script_fd.read():
+      try:
+        content = wrapper_script_fd.read()
+      except:
+        # Fail safe for unreadable files
+        continue
+      if 'LLVMFuzzerTestOneInput' not in content:
         continue
 
-    # Create relevant .jar file if it is not there.
-    jar_file = classfile.replace(".class", ".jar")
-    if not os.path.exists(jar_file):
-      subprocess.check_call("jar cvf %s %s" % (jar_file, classfile), shell=True)
+      # If wrapper script exists, retrieve the java class with package name
+      classname = content.split("--target_class=")[1].split(" ")[0]
+      targets.append(classname)
 
-    targets.append(classfile)
+      # If classfile exists, pack it to jar file
+      for root, _, files in os.walk(path):
+        for file in files:
+          class_location = "%s.class" % classname.replace(".", "/")
+          full_path = os.path.join(root, file)
+          if full_path.endswith(class_location):
+            if "/" in class_location:
+              newdir = os.path.join(path, class_location.rsplit("/", 1)[0])
+              if not os.path.exists(newdir):
+                os.makedirs(newdir, exist_ok=True)
+              os.rename(full_path, os.path.join(path, class_location))
+            class_file_list.append(os.path.join(path, class_location))
+
+  # Create relevant .jar file for all loose class files
+  subprocess.check_call("jar cvf package.jar %s" %  " ".join(class_file_list), shell=True)
+
   return targets
 
 def get_all_jar_files(path):
@@ -102,9 +117,23 @@ def run_introspector_frontend(target_class, jar_set):
       FI_JVM_BASE + "/" + PLUGIN_PATH,
       CGRAPH_STR,
       jarfile_str,
-      target_class.replace(".class", ""),
+      target_class,
       "fuzzerTestOneInput", # entrymethod
-      "===jdk.:java.:javax.:sun.:sunw.:com.sun.:com.ibm.:com.apple.:apple.awt." # include prefix === exclude prefix
+      """java.lang.Runtime:javax.xml.xpath.XPath:java.lang.Thread:java.lang.Runnable:\
+java.util.concurrent.Executor:java.util.concurrent.Callable:java.lang.System:\
+java.lang.ProcessBuilder===jdk.*:java.*:javax.*:sun.*:sunw.*:com.sun.*:com.ibm.*:\
+com.apple.*:apple.awt.*===[java.lang.Runtime].exec:[javax.xml.xpath.XPath].compile:\
+[javax.xml.xpath.XPath].evaluate:[java.lang.Thread].run:[java.lang.Runnable].run:\
+[java.util.concurrent.Executor].execute:[java.util.concurrent.Callable].call:\
+[java.lang.System].console:[java.lang.System].load:[java.lang.System].loadLibrary:\
+[java.lang.System].apLibraryName:[java.lang.System].runFinalization:\
+[java.lang.System].setErr:[java.lang.System].setIn:[java.lang.System].setOut:\
+[java.lang.System].setProperties:[java.lang.System].setProperty:\
+[java.lang.System].setSecurityManager:[java.lang.ProcessBuilder].directory:\
+[java.lang.ProcessBuilder].inheritIO:[java.lang.ProcessBuilder].command:\
+[java.lang.ProcessBuilder].redirectError:[java.lang.ProcessBuilder].redirectErrorStream:\
+[java.lang.ProcessBuilder].redirectInput:[java.lang.ProcessBuilder].redirectOutput:\
+[java.lang.ProcessBuilder].start""" # include prefix === exclude prefix === sink functions
   ]
 
   print("Running command: [%s]" % " ".join(cmd))
@@ -126,6 +155,7 @@ def run_analysis(path):
   os.chdir(currwd)
 
 if __name__ == "__main__":
+  print("In oss-fuzz-main")
   if 'MVN' in os.environ:
     target_mvn = os.environ['MVN']
   currdir = os.getcwd()
@@ -133,3 +163,4 @@ if __name__ == "__main__":
   os.chdir(mydir)
   run_analysis(os.environ['OUT'])
   os.chdir(currdir)
+  print("Exit oss-fuzz-main")
