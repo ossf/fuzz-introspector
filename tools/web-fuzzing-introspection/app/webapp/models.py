779936d73e909bafedaee04ff4f67678f95364bb
DavidKorczynski <david@adalogics.com>
2023-05-15 18:33:27 +0100

    webapp: refactor and style (#1019)
    Signed-off-by: David Korczynski <david@adalogics.com>

--- tools/web-fuzzing-introspection/app/webapp/models.py
+++ tools/web-fuzzing-introspection/app/webapp/models.py
@@ -14,28 +14,37 @@
 
 import datetime
 
+
 def get_date_at_offset_as_str(day_offset=-1):
     datestr = (datetime.date.today() +
                datetime.timedelta(day_offset)).strftime("%Y-%m-%d")
     return datestr
 
+
 class DBTimestamp:
+
     def __init__(self, date, project_count, fuzzer_count, function_count):
         self.date = date
         self.project_count = project_count
         self.fuzzer_count = fuzzer_count
         self.function_count = function_count
 
+
 class DBSummary:
-    def __init__(self, all_projects, total_number_of_projects, total_fuzzers, total_functions, language_count):
+
+    def __init__(self, all_projects, total_number_of_projects, total_fuzzers,
+                 total_functions, language_count):
         self.all_projects = all_projects
         self.total_number_of_projects = total_number_of_projects
         self.total_fuzzers = total_fuzzers
         self.total_functions = total_functions
         self.language_count = language_count
 
+
 class ProjectTimestamp:
-    def __init__(self, project_name, date, coverage_lines, coverage_functions, static_reachability, fuzzer_count):
+
+    def __init__(self, project_name, date, coverage_lines, coverage_functions,
+                 static_reachability, fuzzer_count):
         self.project_name = project_name
         # date in the format Y-m-d
         self.date = date
@@ -44,8 +53,11 @@ class ProjectTimestamp:
         self.static_reachability = static_reachability
         self.fuzzer_count = fuzzer_count
 
+
 class Project:
-    def __init__(self, name, language, fuzz_count, reach, runtime_cov, introspector_report_url, code_coverage_report_url):
+
+    def __init__(self, name, language, fuzz_count, reach, runtime_cov,
+                 introspector_report_url, code_coverage_report_url):
         self.name = name
         self.language = language
         self.fuzz_count = fuzz_count
@@ -54,8 +66,17 @@ class Project:
         self.introspector_report_url = introspector_report_url
         self.code_coverage_report_url = code_coverage_report_url
 
+
 class Function:
-    def __init__(self, name, project, is_reached=False, runtime_code_coverage = 0.0, function_filename = "", reached_by_fuzzers=0, code_coverage_url=""):
+
+    def __init__(self,
+                 name,
+                 project,
+                 is_reached=False,
+                 runtime_code_coverage=0.0,
+                 function_filename="",
+                 reached_by_fuzzers=0,
+                 code_coverage_url=""):
         self.name = name
         self.function_filename = function_filename
         self.project = project
