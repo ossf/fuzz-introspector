34d1c29efdb2ecfa4d9fef4740d707abc44f86ab
David Korczynski <david@adalogics.com>
2023-06-13 03:31:34 -0700

    webapp: change name of data storage
    It is currently named "test" whereas this is not really what it is used
    for in practice. Changing the name to be more appropriate to its use as
    it's quite unintuitive atm.
    
    Signed-off-by: David Korczynski <david@adalogics.com>

---
+++ tools/web-fuzzing-introspection/app/webapp/data_storage.py
@@ -0,0 +1,28 @@
+# Auto-generated
+#from app.site.models import *
+from .models import *
+
+
+PROJECT_TIMESTAMPS = []
+
+DB_TIMESTAMPS = []
+
+PROJECTS = []
+
+FUNCTIONS = []
+
+BLOCKERS = []
+
+BUILD_STATUS = []
+
+def get_projects():
+  return PROJECTS
+
+def get_functions():
+  return FUNCTIONS
+
+def get_blockers():
+  return BLOCKERS
+
+def get_build_status():
+  return BUILD_STATUS
