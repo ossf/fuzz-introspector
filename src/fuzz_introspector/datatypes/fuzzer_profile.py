47b0e7101cad56b3e7f3ecb8a03420dbc1eabf4e
David Korczynski <david@adalogics.com>
2023-08-24 15:38:43 -0700

    style nit
    Signed-off-by: David Korczynski <david@adalogics.com>

--- src/fuzz_introspector/datatypes/fuzzer_profile.py
+++ src/fuzz_introspector/datatypes/fuzzer_profile.py
@@ -47,11 +47,11 @@ class FuzzerProfile:
         self.binary_executable: str = ""
         self.file_targets: Dict[str, Set[str]] = dict()
         self.coverage: Optional[code_coverage.CoverageProfile] = None
-        self.all_class_functions: Dict[
-            str, function_profile.FunctionProfile] = dict()
+        self.all_class_functions: Dict[str,
+                                       function_profile.FunctionProfile] = dict()
         self.branch_blockers: List[Any] = []
-        self.dst_to_fd_cache: Dict[
-            str, function_profile.FunctionProfile] = dict()
+        self.dst_to_fd_cache: Dict[str,
+                                   function_profile.FunctionProfile] = dict()
         self.dst_to_fd_cache_set: bool = False
 
         self._target_lang = target_lang
