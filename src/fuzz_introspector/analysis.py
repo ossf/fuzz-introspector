7cf55dc45d9b3c7d1949dac62f7b9453b8e76316
David Korczynski <david@adalogics.com>
2023-08-25 06:12:03 -0700

    analysis: reduce expensive loops
    Signed-off-by: David Korczynski <david@adalogics.com>

--- src/fuzz_introspector/analysis.py
+++ src/fuzz_introspector/analysis.py
@@ -295,13 +295,24 @@ def get_url_to_cov_report(profile, node, target_coverage_url):
                                 node.dst_function_name)
     ]
     for dst in dst_options:
-        for fd_k, fd in profile.all_class_functions.items():
-            if (fd.function_name == dst or utils.normalise_str(
-                    fd.function_name) == utils.normalise_str(dst)):
-                return profile.resolve_coverage_link(target_coverage_url,
-                                                     fd.function_source_file,
-                                                     fd.function_linenumber,
-                                                     fd.function_name)
+        try:
+            fd = profile.dst_to_fd_cache[dst]
+            return profile.resolve_coverage_link(target_coverage_url,
+                                                 fd.function_source_file,
+                                                 fd.function_linenumber,
+                                                 fd.function_name)
+        except KeyError:
+            pass
+
+        try:
+            fd = profile.dst_to_fd_cache[utils.normalise_str(dst)]
+            return profile.resolve_coverage_link(target_coverage_url,
+                                                 fd.function_source_file,
+                                                 fd.function_linenumber,
+                                                 fd.function_name)
+        except KeyError:
+            pass
+
     return "#"
 
 
@@ -311,16 +322,24 @@ def get_parent_callsite_link(node, callstack, profile, target_coverage_url):
         parent_fname = callstack_get_parent(node, callstack)
         dst_options = [parent_fname, utils.demangle_cpp_func(parent_fname)]
         for dst in dst_options:
-            for fd_k, fd in profile.all_class_functions.items():
-                if (utils.demangle_jvm_func(fd.function_source_file,
-                                            fd.function_name) == dst
-                        or utils.normalise_str(
-                            fd.function_name) == utils.normalise_str(dst)):
-                    callsite_link = profile.resolve_coverage_link(
-                        target_coverage_url, fd.function_source_file,
-                        node.src_linenumber, fd.function_name)
-                    return callsite_link
+            # First try the cache
+            try:
+                fd = profile.dst_to_fd_cache[dst]
+                callsite_link = profile.resolve_coverage_link(
+                    target_coverage_url, fd.function_source_file,
+                    node.src_linenumber, fd.function_name)
+                return callsite_link
+            except KeyError:
+                pass
 
+            try:
+                fd = profile.dst_to_fd_cache[utils.normalise_str(dst)]
+                callsite_link = profile.resolve_coverage_link(
+                    target_coverage_url, fd.function_source_file,
+                    node.src_linenumber, fd.function_name)
+                return callsite_link
+            except KeyError:
+                pass
     return "#"
 
 
