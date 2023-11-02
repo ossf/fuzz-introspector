aca3741bb6a0c489ab734bab4fd6beff6d39d463
Arthur Chan <arthur.chan@adalogics.com>
2023-11-02 17:27:19 +0000

    Unify line indentation
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- tools/auto-fuzz/templates/python/fuzz_1.py
+++ tools/auto-fuzz/templates/python/fuzz_1.py
@@ -20,14 +20,14 @@ import atheris
 
 @atheris.instrument_func
 def TestOneInput(data):
-  fdp = atheris.FuzzedDataProvider(data)
+    fdp = atheris.FuzzedDataProvider(data)
 
 
 def main():
-  atheris.instrument_all()
-  atheris.Setup(sys.argv, TestOneInput)
-  atheris.Fuzz()
+    atheris.instrument_all()
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
 
 
 if __name__ == "__main__":
-  main()
+    main()
