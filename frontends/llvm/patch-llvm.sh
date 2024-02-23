46ca2bf6f1e1332d650d27845d37c5118a1a2770
DavidKorczynski <david@adalogics.com>
2024-02-23 12:47:18 +0000

    llvm: upgrade to 18 (#1439)
    * llvm: provide minimum changes needed for llvm 18 update
    
    These changes make it possible to build against llvm 18.
    
    In essence, there does not seem to be big problems. The only thing we
    need to adjust is how we identify indirect calls done by way of VTables.
    In essence we don't rely on types in function signatures from the LLVM
    IR anymore as we want to use debug information for this anyways, so we
    can kind of skip that part.
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    * frontend: llvm: adjust patch
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    * oss-fuzz: adjust patch
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    * test: remove legacy pass manager since we do not use it anymore
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    * oss-fuzz: add diff used for testing llvm18 in OSS-Fuzz
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    ---------
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- frontends/llvm/patch-llvm.sh
+++ frontends/llvm/patch-llvm.sh
@@ -17,11 +17,9 @@
 set -x
 
 echo "add_subdirectory(FuzzIntrospector)" >> ./llvm/lib/Transforms/CMakeLists.txt
-sed -i 's/whole-program devirtualization and bitset lowering./whole-program devirtualization and bitset lowering.\nPM.add(createFuzzIntrospectorPass());/g' ./llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
-sed -i 's/using namespace/#include "llvm\/Transforms\/FuzzIntrospector\/FuzzIntrospector.h"\nusing namespace/g' ./llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
 sed -i 's/Instrumentation/Instrumentation\n  FuzzIntrospector/g' ./llvm/lib/Transforms/IPO/CMakeLists.txt
 
-sed -i 's/void initializeCrossDSOCFIPass(PassRegistry\&);/void initializeCrossDSOCFIPass(PassRegistry\&);\nvoid initializeFuzzIntrospectorPass(PassRegistry\&);/g' ./llvm/include/llvm/InitializePasses.h
+sed -i 's/void initializeXRayInstrumentationPass(PassRegistry\&);/void initializeXRayInstrumentationPass(PassRegistry\&);\nvoid initializeFuzzIntrospectorPass(PassRegistry\&);/g' ./llvm/include/llvm/InitializePasses.h
 sed -i 's/#include "llvm\/Transforms\/Instrumentation\/ThreadSanitizer.h"/#include "llvm\/Transforms\/Instrumentation\/ThreadSanitizer.h"\n#include "llvm\/Transforms\/FuzzIntrospector\/FuzzIntrospector.h"/g' ./llvm/lib/Passes/PassBuilder.cpp
 sed -i 's/#include "llvm\/Transforms\/Instrumentation\/PGOInstrumentation.h"/#include "llvm\/Transforms\/Instrumentation\/PGOInstrumentation.h"\n#include "llvm\/Transforms\/FuzzIntrospector\/FuzzIntrospector.h"/g' ./llvm/lib/Passes/PassBuilderPipelines.cpp
 sed -i 's/MPM.addPass(CrossDSOCFIPass());/MPM.addPass(CrossDSOCFIPass());\n  MPM.addPass(FuzzIntrospectorPass());/g' ./llvm/lib/Passes/PassBuilderPipelines.cpp
