f27785391cc9a5cf5e6bddb3afb897743f409768
fanquake <fanquake@gmail.com>
2024-03-05 15:56:44 +0000

    examples: remove `-flegacy-pass-manager` usage (#1461)
    examples: remove -flegacy-pass-manager usage
    
    Otherwise you'll see:
    `clang: error: unknown argument: '-flegacy-pass-manager'`
    when running through localbuild.md. LLVM 18 has been used since #1440.
    
    Signed-off-by: fanquake <fanquake@gmail.com>

--- tests/simple-example-0/build_cov.sh
+++ tests/simple-example-0/build_cov.sh
@@ -21,7 +21,7 @@ if [ -d "work" ]; then
     cd work
 fi
 
-../../../build/llvm-build/bin/clang -flegacy-pass-manager -fprofile-instr-generate -fcoverage-mapping -fsanitize=fuzzer -g ../fuzzer.c -o fuzzer
+../../../build/llvm-build/bin/clang -fprofile-instr-generate -fcoverage-mapping -fsanitize=fuzzer -g ../fuzzer.c -o fuzzer
 ./fuzzer -max_total_time=3
 ../../../build/llvm-build/bin/llvm-profdata merge -sparse default.profraw -o merged_cov.profdata
 ../../../build/llvm-build/bin/llvm-cov show -instr-profile=merged_cov.profdata -object=./fuzzer -line-coverage-gt=0 > fuzzer.covreport
