30f44a102c81b9c12d57120fefc82d50cf6287c7
Arthur Chan <arthur.chan@adalogics.com>
2024-05-10 20:01:06 +0000

    Add separate storage for skipped jvm constructors
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- src/fuzz_introspector/datatypes/fuzzer_profile.py
+++ src/fuzz_introspector/datatypes/fuzzer_profile.py
@@ -555,13 +555,15 @@ class FuzzerProfile:
                     f"May have non-normalised function: {elem['functionName']}"
                 )
 
-            if self.target_lang == "jvm" and "<init>" in elem['functionName']:
-                logger.debug("Skipping <init> method for JVM")
-                continue
-
             func_profile = function_profile.FunctionProfile(elem)
             logger.debug(f"Adding {func_profile.function_name}")
-            self.all_class_functions[func_profile.function_name] = func_profile
+
+            if self.target_lang == "jvm" and "<init>" in elem['functionName']:
+                # Store JVM constructor separately
+                self.all_class_constructors[func_profile.function_name] = func_profile
+            else:
+                # Store the functions
+                self.all_class_functions[func_profile.function_name] = func_profile
 
     def _is_func_name_missing_normalisation(self, func_name: str) -> bool:
         if "." in func_name:
