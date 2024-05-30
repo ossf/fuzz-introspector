efe7552d0d51eeecf9ec0e0ad673aebe94084657
DavidKorczynski <david@adalogics.com>
2024-05-30 21:03:26 +0100

    oss-fuzz-gen: extend runner script (#1536)
    Signed-off-by: David Korczynski <david@adalogics.com>

--- scripts/oss-fuzz-gen-e2e/run_all.sh
+++ scripts/oss-fuzz-gen-e2e/run_all.sh
@@ -15,18 +15,31 @@
 #
 ################################################################################
 
-BASE_DIR=$PWD                                                                  
+BASE_DIR=$PWD/workdir
 FI_DIR=$BASE_DIR/fuzz-introspector/
-PROJECT=${PROJECT}
+BENCHMARK_HEURISTICS="${VARIABLE:-far-reach-low-coverage}"
 OSS_FUZZ_GEN_MODEL=${MODEL}
+PROJECT=${@}
+
+comma_separated=""
+for proj in ${PROJECT}; do
+  echo ${proj}
+  comma_separated="${comma_separated}${proj},"
+done
+comma_separated=${comma_separated::-1}
+
 # Launch virtualenv
+cd ${BASE_DIR}
 . .venv/bin/activate
 
+echo "[+] Creating introspector reports"
 cd $FI_DIR/oss_fuzz_integration/oss-fuzz                                        
-python3 ../runner.py introspector $PROJECT 10 --disable-webserver                
+for project in ${PROJECT}; do
+  python3 ../runner.py introspector $project 10 --disable-webserver
+done
                                                                                
-                                                                               
-# Create webserver DB                                                          
+# Create webserver DB
+echo "[+] Creating the webapp DB"
 cd $FI_DIR/tools/web-fuzzing-introspection/app/static/assets/db/
 python3 ./web_db_creator_from_summary.py \
     --local-oss-fuzz $FI_DIR/oss_fuzz_integration/oss-fuzz                      
@@ -35,7 +48,7 @@ python3 ./web_db_creator_from_summary.py \
 echo "Shutting down server in case it's running"
 curl --silent http://localhost:8080/api/shutdown || true
 
-echo "Launching FI webapp"
+echo "[+] Launching FI webapp"
 cd $FI_DIR/tools/web-fuzzing-introspection/app/                                
 FUZZ_INTROSPECTOR_LOCAL_OSS_FUZZ=$FI_DIR/oss_fuzz_integration/oss-fuzz \
   python3 ./main.py >> /dev/null &
@@ -49,20 +62,19 @@ do
     echo "Found it"
     break
   fi
-  echo "Sleeping"
+  echo "- Waiting for webapp to load. Sleeping ${SECONDS} seconds."
   sleep ${SECONDS}
-
 done
 
 # Deactivate
-echo "Running OSS-Fuzz-gen experiment"
+echo "[+] Running OSS-Fuzz-gen experiment"
 cd $BASE_DIR/oss-fuzz-gen
 ./run_all_experiments.py \
-  --model=$OSS_FUZZ_GEN_MODEL \
-  -g low-cov-with-fuzz-keyword,far-reach-low-coverage \
-  -gp $PROJECT \
-  -gm 6 \
-  -e http://127.0.0.1:8080/api
+    --model=$OSS_FUZZ_GEN_MODEL \
+    -g ${BENCHMARK_HEURISTICS} \
+    -gp ${comma_separated} \
+    -gm 6 \
+    -e http://127.0.0.1:8080/api
 
 echo "Shutting down started webserver"
 curl --silent http://localhost:8080/api/shutdown || true
