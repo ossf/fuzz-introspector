25664203df9e184e4fc8aa05ee912e6d1ed282d0
Arthur Chan <arthur.chan@adalogics.com>
2024-06-24 11:01:04 +0000

    Fix formatting
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- src/fuzz_introspector/html_report.py
+++ src/fuzz_introspector/html_report.py
@@ -804,6 +804,7 @@ def create_html_report(introspection_proj: analysis.IntrospectionProject,
     if introspection_proj.language == 'jvm':
         source_file_list = [
             func_item['Functions filename']
-            for func_item in (all_functions_json_report + jvm_constructor_json_report)
+            for func_item in (all_functions_json_report +
+                              jvm_constructor_json_report)
         ]
         utils.copy_java_source_files(source_file_list)
