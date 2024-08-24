f05cf9b83794ebd9af4d1682647b6196e2725862
DavidKorczynski <david@adalogics.com>
2024-08-24 13:27:09 +0100

    debug_info: extend syzkaller types
    Signed-off-by: DavidKorczynski <david@adalogics.com>

--- src/fuzz_introspector/debug_info.py
+++ src/fuzz_introspector/debug_info.py
@@ -579,7 +579,7 @@ def extract_syzkaller_type(param_list):
                 syzkaller_tag = 'int32'
             elif param == '__u64' or param == 's64':
                 syzkaller_tag = 'int64'
-            elif param == '__u8' or param == 'u8':
+            elif param == '__u8' or param == 'u8' or param == '__s8':
                 syzkaller_tag = 'int8'
             elif param == '__u16' or param == 'u16':
                 syzkaller_tag = 'int16'
