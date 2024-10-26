714c9177e2e3ff56df94e14f014d4d6fe9e3d5b5
David Korczynski <david@adalogics.com>
2024-10-26 16:17:50 -0700

    webapp: clean up API doc page
    Also set swagger api doc to be at `api-doc`
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- tools/web-fuzzing-introspection/app/main.py
+++ tools/web-fuzzing-introspection/app/main.py
@@ -27,7 +27,7 @@ def create_app():
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/api-doc"
     app.config[
         "OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.register_blueprint(routes.blueprint)
