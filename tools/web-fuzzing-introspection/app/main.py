cd16e60fe84b79d349602207b84e1dadc5bb2226
David Korczynski <david@adalogics.com>
2024-10-26 07:46:00 -0700

    webapp: enable automated API documentation
    Introduces API documentation with flask smorest. This has been applied
    to a single API for now to introduce the concept. Follow-up PR will
    happen with all APIs.
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- tools/web-fuzzing-introspection/app/main.py
+++ tools/web-fuzzing-introspection/app/main.py
@@ -15,7 +15,7 @@
 import os
 
 from flask import Flask
-
+from flask_smorest import Api
 from typing import Optional, Tuple
 import webapp
 from webapp import routes
@@ -23,7 +23,17 @@ from webapp import routes
 
 def create_app():
     app = Flask(__name__)
+    app.config["API_TITLE"] = "Fuzz Introspector Web API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.register_blueprint(routes.blueprint)
+
+    api = Api(app)
+    api.register_blueprint(routes.api_blueprint)
 
     try:
         routes.gtag = os.environ['G_ANALYTICS_TAG']
