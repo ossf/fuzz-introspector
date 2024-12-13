dcce6ead573dfc3abe2a9976798dab49d1e79b1a
David Korczynski <david@adalogics.com>
2024-12-13 22:30:24 +0000

    frontend: add module to expose core operations
    Signed-off-by: David Korczynski <david@adalogics.com>

---
+++ src/fuzz_introspector/frontends/core.py
@@ -0,0 +1,40 @@
+# Copyright 2024 Fuzz Introspector Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""APIs that make it easy to access frontend analysis."""
+
+import logging
+
+from fuzz_introspector.frontends.src import frontend_c
+
+logger = logging.getLogger(name=__name__)
+
+
+def analyse_source_file(code: bytes, language: str):
+    """Runs frontend analysis on a code snippet.
+    
+    The code snippet should correspond to what you'd normally find in
+    a source file, e.g. a number of functions, include statements and so
+    on.
+    
+    Returns a frontend Source code module if successful and None otherwise."""
+
+    if language == 'c':
+        return frontend_c.analyse_source_code(code)
+    elif language == 'cpp':
+        logger.info('CPP is not implement yet.')
+    elif language == 'go':
+        logger.info('Go is not implement yet.')
+    else:
+        logger.info('Language %s not supported', language)
+    return None
