49250d1d4ac079a26a03899ac6f2798a36223c8f
DavidKorczynski <david@adalogics.com>
2024-12-27 10:56:16 +0000

    core: add end to end CLI command (#1920)
    * core: add end to end CLI command
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    * fix unit test
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    * nit
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    * nit
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    * fix cflite
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    * fix cflite
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    * bump mypy
    
    Signed-off-by: David Korczynski <david@adalogics.com>
    
    ---------
    
    Signed-off-by: David Korczynski <david@adalogics.com>

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -eu
 python3 -m pip install --upgrade pip
 python3 -m pip install jaraco.text==3.12.0
-python3 -m pip install --prefer-binary -r requirements.txt
+cd src
+python3 -m pip install -e .
+cd ../
 
 cp src/test/fuzz/*.py .
 
@@ -9,6 +11,11 @@ hidden_imports="--hidden-import=yaml \
                --hidden-import=cxxfilt \
                --hidden-import=json \
                --hidden-import=bs4 \
+               --hidden-import=tree_sitter \
+               --hidden-import=tree_sitter_cpp \
+               --hidden-import=tree_sitter_c \
+               --hidden-import=tree_sitter_go \
+               --hidden-import=tree_sitter_java \
                --hidden-import=pkg_resources.extern \
                --hidden-import=rust_demangler"
 #fuzzers="test_fuzz_cfg_load.py test_fuzz_report_generation.py"
