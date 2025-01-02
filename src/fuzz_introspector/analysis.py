bee4d4eee702fc073aadc7e3a3f833aed28b4377
David Korczynski <david@adalogics.com>
2025-01-02 03:21:06 -0800

    core: minor refactoring
    Signed-off-by: David Korczynski <david@adalogics.com>

--- src/fuzz_introspector/analysis.py
+++ src/fuzz_introspector/analysis.py
@@ -64,7 +64,7 @@ class IntrospectionProject():
                                                       self.language,
                                                       parallelise)
 
-        logger.info(f"Found {len(self.profiles)} profiles")
+        logger.info(f"Found %d profiles", len(self.profiles))
         if len(self.profiles) == 0:
             logger.info("Found no profiles")
             raise DataLoaderError("No fuzzer profiles")
