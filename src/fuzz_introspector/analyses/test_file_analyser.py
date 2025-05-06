1ea3b122f6425374c5f30bb7d245d02556e76a40
Arthur Chan <arthur.chan@adalogics.com>
2025-05-07 00:12:02 +0100

    test_file_analyser: Initialise standalone test file analyser (#2144)
    * test_file_analyser: Initialise standalone test file analyser
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>
    
    * Fix formatting
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>
    
    * Fix formatting
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>
    
    * Fix formatting
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>
    
    ---------
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

---
+++ src/fuzz_introspector/analyses/test_file_analyser.py
@@ -0,0 +1,98 @@
+# Copyright 2025 Fuzz Introspector Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Analysis plugin for analysing test files."""
+import json
+import logging
+import os
+
+from typing import (Any, List, Dict)
+
+from fuzz_introspector import (analysis, html_helpers)
+
+from fuzz_introspector.datatypes import (project_profile, fuzzer_profile)
+
+logger = logging.getLogger(name=__name__)
+
+
+class TestFileAnalyser(analysis.AnalysisInterface):
+    """Analysis utility for testing analysis."""
+
+    name: str = 'TestFileAnalyser'
+
+    def __init__(self) -> None:
+        self.json_results: Dict[str, Any] = {}
+        self.json_string_result = ''
+
+    @classmethod
+    def get_name(cls):
+        """Return the analyser identifying name for processing.
+
+        :return: The identifying name of this analyser
+        :rtype: str
+        """
+        return cls.name
+
+    def get_json_string_result(self) -> str:
+        """Return the stored json string result.
+
+        :return: The json string result processed and stored
+            by this analyser
+        :rtype: str
+        """
+        if self.json_string_result:
+            return self.json_string_result
+
+        return json.dumps(self.json_results)
+
+    def set_json_string_result(self, json_string: str):
+        """Store the result of this analyser as json string result
+        for further processing in a later time.
+
+        :param json_string: A json string variable storing the
+            processing result of the analyser for future use
+        :type json_string: str
+        """
+        self.json_string_result = json_string
+
+    def set_base_information(self, directory: str, language: str):
+        """Setter for base information."""
+        self.directory = directory
+        self.language = language
+
+    def analysis_func(self,
+                      table_of_contents: html_helpers.HtmlTableOfContents,
+                      tables: List[str],
+                      proj_profile: project_profile.MergedProjectProfile,
+                      profiles: List[fuzzer_profile.FuzzerProfile],
+                      basefolder: str, coverage_url: str,
+                      conclusions: List[html_helpers.HTMLConclusion],
+                      out_dir: str) -> str:
+        """Analysis function."""
+        self.standalone_analysis(proj_profile, profiles, out_dir)
+        return ''
+
+    def standalone_analysis(self,
+                            proj_profile: project_profile.MergedProjectProfile,
+                            profiles: List[fuzzer_profile.FuzzerProfile],
+                            out_dir: str) -> None:
+        """Standalone analysis."""
+        super().standalone_analysis(proj_profile, profiles, out_dir)
+
+        all_test_files: set[str] = analysis.extract_tests_from_directories(
+            {self.directory}, self.language, out_dir)
+
+        with open(os.path.join(out_dir, 'all_tests.json'), 'w') as f:
+            f.write(json.dumps(list(all_test_files)))
+
+        return None
--- src/fuzz_introspector/cli.py
+++ src/fuzz_introspector/cli.py
@@ -259,6 +259,26 @@ def get_cmdline_parser() -> argparse.ArgumentParser:
         type=str,
         help='Folder to store analysis results.')
 
+    test_file_analyser_parser = analyser_parser.add_parser(
+        'TestFileAnalyser',
+        help=('Provide analysis of public test files found in the project.'))
+
+    test_file_analyser_parser.add_argument(
+        '--target-dir',
+        type=str,
+        help='Directory holding source to analyse.',
+        required=True)
+    test_file_analyser_parser.add_argument(
+        '--language',
+        type=str,
+        help='Programming of the source code to analyse.',
+        choices=constants.LANGUAGES_SUPPORTED)
+    test_file_analyser_parser.add_argument(
+        '--out-dir',
+        default='',
+        type=str,
+        help='Folder to store analysis results.')
+
     return parser
 
 
