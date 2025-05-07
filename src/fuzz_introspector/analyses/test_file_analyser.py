cf530d615523aea1fcfeea26877108cef96d1519
Arthur Chan <arthur.chan@adalogics.com>
2025-05-07 15:53:14 +0000

    analyser: Fix logic of test file analyser
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- src/fuzz_introspector/analyses/test_file_analyser.py
+++ src/fuzz_introspector/analyses/test_file_analyser.py
@@ -33,6 +33,8 @@ class TestFileAnalyser(analysis.AnalysisInterface):
     def __init__(self) -> None:
         self.json_results: Dict[str, Any] = {}
         self.json_string_result = ''
+        self.directory = ''
+        self.language = ''
 
     @classmethod
     def get_name(cls):
@@ -67,7 +69,7 @@ class TestFileAnalyser(analysis.AnalysisInterface):
 
     def set_base_information(self, directory: str, language: str):
         """Setter for base information."""
-        self.directory = directory
+        self.directory = os.path.abspath(directory)
         self.language = language
 
     def analysis_func(self,
@@ -88,11 +90,63 @@ class TestFileAnalyser(analysis.AnalysisInterface):
                             out_dir: str) -> None:
         """Standalone analysis."""
         super().standalone_analysis(proj_profile, profiles, out_dir)
+        functions = proj_profile.get_all_functions_with_source()
 
-        all_test_files: set[str] = analysis.extract_tests_from_directories(
-            {self.directory}, self.language, out_dir)
+        test_files = set()
+        if os.path.isfile(os.path.join(out_dir, 'all_tests.json')):
+            with open(os.path.join(out_dir, 'all_tests.json'), 'r') as f:
+                test_files = set(json.load(f))
 
+        # Auto determine base information if not provided
+        if not self.directory:
+            paths = [func.function_source_file for func in functions.values()]
+            self.directory = os.path.commonpath(paths)
+
+        if not self.language:
+            self.language = proj_profile.language
+
+        test_files.update(analysis.extract_tests_from_directories(
+            {self.directory}, self.language, out_dir, False))
+
+        # Get all functions within test files
+        test_functions: dict[str, list[dict[str, object]]] = {}
+        seen_functions: dict[str, set[tuple[str, str]]] = {}
+        for function in functions.values():
+            test_source = function.function_source_file
+
+            # Skip unrelated functions
+            if test_source not in test_files:
+                continue
+
+            if test_source not in test_functions:
+                test_functions[test_source] = []
+                seen_functions[test_source] = set()
+
+            for reached_name in function.functions_reached:
+                reached = functions.get(reached_name)
+
+                # Skip other test functions or external functions
+                if not reached or reached.function_source_file in test_files:
+                    continue
+
+                key = (reached.function_name, reached.function_source_file)
+
+                # Skip duplicated, reached funcitons
+                if key in seen_functions[test_source]:
+                    continue
+
+                seen_functions[test_source].add(key)
+                test_functions[test_source].append(reached.to_dict())
+
+        # Remove useless test files
+        test_functions = {k: v for k, v in test_functions.items() if v}
+
+        # Store test files
         with open(os.path.join(out_dir, 'all_tests.json'), 'w') as f:
-            f.write(json.dumps(list(all_test_files)))
+            f.write(json.dumps(list(test_files)))
+
+        # Store test files with cross reference information
+        with open(os.path.join(out_dir, 'all_tests_with_xreference.json'), 'w') as f:
+            f.write(json.dumps(test_functions))
 
         return None
